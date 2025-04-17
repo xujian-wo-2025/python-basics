6786c538d49afb81683b48b6d0baf2e83526d23e
123 <15236883+xujian123456123@user.noreply.gitee.com>
2025-04-17 16:05:11 +0800

    运算符

---
+++ python_06.py
@@ -0,0 +1,18 @@
+# 运算符
+
+
+# 算术运算符
+print("1 + 1 = ", 1 + 1)
+print("1 - 1 = ", 1 - 1)
+print("1 * 1 = ", 1 * 1)
+print("1 / 1 = ", 1 / 1)
+print("1 % 1 = ", 1 % 1)
+
+
+# 赋值运算符
+num = 10
+print(num)
+num = 10 + 1
+print(num)
+num += 1
+print(num)
