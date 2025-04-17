345d719487b51ec7b4b3310b3e35b2814a6682c2
123 <15236883+xujian123456123@user.noreply.gitee.com>
2025-04-17 21:52:48 +0800

    range的使用

---
+++ python_15.py
@@ -0,0 +1,11 @@
+# range语法1 range(num)
+for x in range(10):
+    print(x)
+
+# range语法2 range(num1,num2)
+for x in range(5,10):
+    print(x)
+
+# range语法3 range(num1,num2,step)
+for x in range(5,10,2):
+    print(x)
