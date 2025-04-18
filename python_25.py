94bc4c264bb33dcf0efc53a4b6b3df9022796fa2
123 <15236883+xujian123456123@user.noreply.gitee.com>
2025-04-18 21:04:12 +0800

    字符串的常用操作

---
+++ python_25.py
@@ -0,0 +1,17 @@
+my_str = "itheima and itcast"
+print(my_str[2])   # h
+
+# index方法
+print(my_str.index('and'))   # 8
+
+# replace方法
+print(my_str.replace('it', '程序')) # 程序heima and 程序cast
+
+# split方法
+my_str = "hello python itheima itcast"
+print(my_str.split(' ')) # ['hello', 'python', 'itheima', 'itcast']
+
+# strip方法
+my_str = "     itheima is itcast "
+print(my_str.strip())   # itheima is itcast
+
